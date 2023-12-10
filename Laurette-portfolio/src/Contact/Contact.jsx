13a3e7d282108a35f1da532dec9f1bf75263c3a4
Thintalltom <adeyanjutomide@gmail.com>
2023-12-10 03:48:50 +0100

    updated the site

--- Laurette-portfolio/src/Contact/Contact.jsx
+++ Laurette-portfolio/src/Contact/Contact.jsx
@@ -3,16 +3,7 @@ import { FaTwitterSquare, FaLinkedinIn, FaInstagram,  FaFacebookF } from "react-
 const Contact = () => {
   return (
     <div className='mt-[15rem] '>
-      <div className='flex justify-center items-center flex-col'>
-      <p className='font-extralight'>Subscribe for Newsletter</p>
-      <div className='flex'>
-        <div className='bg-zinc-950 p-[15px]'>
-      <input type='text' defaultValue='Enter Email'className='w-[400px] bg-transparent mt-[10px]'/>
-      <button className='bg-slate-750 '>Subscribe</button>
-      </div>
-      </div>
-
-      </div>
+      
   
 
         <p className='text-center mt-[20px] text-zinc-400 font-light'>
--- Laurette-portfolio/src/Home/Home.jsx
+++ Laurette-portfolio/src/Home/Home.jsx
@@ -6,11 +6,12 @@ import Works from "../Works/Works";
 import Services from "../Services/Services";
 import Contact from "../Contact/Contact";
 import Experience from "../Experience/Experience";
+
 const Home = () => {
   return (
     <div className="">
       <div className="bgZinc text-white">
-        <div className=" p-[50px]  backImg gap-4 bg-zinc-950 flex text-white xs:h-[40rem] 2xl:h-[70rem] xl:h-[70rem] lg:h-[60rem] sm:h-[30rem] ">
+        <div className=" p-[50px]   backImg gap-4 bg-zinc-950 flex text-white xs:h-[40rem] 2xl:h-[70rem] xl:h-[70rem] lg:h-[60rem] sm:h-[30rem] ">
           <div className=" lg:w-[80rem] md:w-[60rem] sm:w-[80rem]  mt-[60px] sm:h-[300px] md:h-[400px] lg:h-[700px]  flex-col flex justify-center items-center">
             <motion.p
             initial={{x: -100}}
@@ -26,6 +27,7 @@ const Home = () => {
             <button className=" hover:border-0 hover:bg-zinc-950 font-extralight border-[0.2px] lg:w-[500px] lg:h-[300px] sm:w-[400px] sm:h-[100px] md:h-[100px]  lg:text-5xl p-4 theBtn">
               HIRE ME
             </button>
+          
           </div>
         </div>
         <div className="p-4 mt-[-60px] h-[50rem]">
