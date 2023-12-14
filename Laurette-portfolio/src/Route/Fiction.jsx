f05389c6fe6004b70f82ab713114af0f4dc26d83
Thintalltom <adeyanjutomide@gmail.com>
2023-12-14 18:47:01 +0100

    achnages made

--- Laurette-portfolio/src/Route/Fiction.jsx
+++ Laurette-portfolio/src/Route/Fiction.jsx
@@ -12,11 +12,15 @@ const Fiction = () => {
       {fiction && fiction.map((fiction, index) => (
         <div key={fiction.author} className='gap-4 p-4'>
           <p className='font-bold'> Title:{fiction.name}</p>
-      
+          <img src={fiction.image} className='w-[200px]' />
           <p className='font-bold'> Author: {fiction.author}</p>
           <p className='font-bold'>Description: {fiction.Description}</p>
           <p>{fiction.body}</p>
-         
+          <button className='bg-zinc-950 p-[10px] rounded-[5px]'>
+          <a href={fiction.file.asset._ref} download className='cursor-pointer text-white'>
+               Download File
+              </a>
+              </button>
           <p className='font-bold '> Date of publication: {fiction.publishedAt}</p>
         
         </div>
