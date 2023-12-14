5e609af37aa968a65c8b1e4908343082fef212ca
Thintalltom <adeyanjutomide@gmail.com>
2023-12-14 18:41:55 +0100

    made changes

--- Laurette-portfolio/src/client.js
+++ Laurette-portfolio/src/client.js
@@ -3,7 +3,8 @@ import {createClient} from '@sanity/client';
 const client = createClient({
     projectId: 'qcqvl705',
     dataset: 'production',
-    useCdn: false
+    useCdn: false,
+    apiVersion: '2023-12-12'
 });
 
 export default client;
